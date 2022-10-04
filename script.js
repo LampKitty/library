b4b07474d01a907211287e37fe7646b0c2086788
Ranpukatu <ranpukatu@gmail.com>
2022-10-04 15:52:07 +0300

    make popup template

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
+const bookShelf = document.querySelector('.bookshelf');
+const styleProperties = 'background-image: url(\'img/bookshelf.png\'); \
+background-size: 250px; \
+box-shadow: 0px 0px 25px 3px; \
+flex: initial;\
+margin: 120px;\
+height: 250px; \
+width: 250px;';
+
 let mylibrary = [];
 
-function Book() {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 function loopBook(arr) {
     for(book in arr) {
+        const content = document.createElement('div');
+        content.setAttribute('style', styleProperties);
+        bookShelf.appendChild(content);
         
+        console.log('book')
     }
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(title, author, pages) {
+        mylibrary.push(new Book(title, author, pages))
 }
+
+addBookToLibrary('Hobbit', 'Tolkien', '256');
+addBookToLibrary('Hobbit', 'Tolkien', '256');
+addBookToLibrary('Hobbit', 'Tolkien', '256');
+addBookToLibrary('Hobbit', 'Tolkien', '256');
+
+
+loopBook(mylibrary);
+console.log(mylibrary);
+
+
+
 
